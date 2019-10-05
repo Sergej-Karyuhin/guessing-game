81d3e6e490bf5450246e6dd4280832171ef77ba9
Sergej <karyuhin.sergej@gmail.com>
2019-10-06 00:15:07 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,17 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
     guess() {
-
+      this.expression = Math.ceil((this.min + this.max) / 2);
+      return this.expression;
     }
-
     lower() {
-
+      this.max = this.expression;
     }
-
     greater() {
-
+      this.min = this.expression;
     }
 }
 
